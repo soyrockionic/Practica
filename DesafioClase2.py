428d14e3c25b2e13d464f7201b238713d7201113
soyrockionic <soyrockionic@gmail.com>
2022-03-21 18:47:42 -0300

    Modifico DesafioClase2

--- DesafioClase2.py
+++ DesafioClase2.py
@@ -1,6 +1,3 @@
 for i in range(4) :
     palabra = input("Ingrese una palabra ")
-    if "r" in palabra :
-        print("TIENE R")
-    else
-        print("NO TIENE R")
+    print("TIENE R" if "r" in palabra else "NO TIENE R")

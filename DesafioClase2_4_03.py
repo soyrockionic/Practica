2c35781cb16f332f1ebd8cd33f7b4b5824f58743
soyrockionic <soyrockionic@gmail.com>
2022-03-23 12:48:17 -0300

    Creo DesafioClase2_4_03

---
+++ DesafioClase2_4_03.py
@@ -0,0 +1,41 @@
+def ingreso_notas() :
+
+    nombre = input("Ingresa un nombre 'fin' para finalizar ")
+    dicci = {}
+    while nombre != "fin" :
+        nota = int(input(f"Ingresa la nota de {nombre} "))
+        dicci[nombre] = nota
+        nombre = input("Ingresa un nombre 'fin' para finalizar ")
+
+    return dicci
+
+
+def calcular_promedio(notas) :
+    
+    suma = 0
+    for i in notas :
+        suma += notas[i]
+
+    return suma / len(notas)
+
+
+def menores_al_prom(notas) :
+    
+    cont = 0
+    for i in notas :
+        if notas[i] < calcular_promedio(notas) :
+            cont += 1
+
+    return cont
+    
+
+notas = ingreso_notas()
+
+print()
+for i in notas :
+    print(i + f" {notas[i]}")
+
+print()
+print(f"Promedio {calcular_promedio(notas):.2f}")
+
+print(f"Cantidad de notas menores a {calcular_promedio(notas):.2f} = {menores_al_prom(notas)}")

80a6dee0657d010801380bd2a02381da3f8eded8
soyrockionic <soyrockionic@gmail.com>
2022-04-07 02:44:24 -0300

    Clase 4 - Probando archivos

---
+++ Clase_04.py
@@ -0,0 +1,28 @@
+arch = open("Nombres.txt", "w")
+archi = open("Notas.txt", "w")
+
+arch.write("Luciana ")
+arch.write("Sofia Juan ")
+arch.write("Walter ")
+archi.write("70 50 37 60")
+
+arch.close()
+archi.close()
+
+def leer() : 
+    arch = open("Nombres.txt", "r")
+    arch1 = arch.read().split()
+    archi = open("Notas.txt", "r")
+    archi1 = archi.read().split()
+    for val1, val2 in zip(arch1, archi1) :
+        print('{:<12}  {:<8}'.format(val1,val2))
+    arch.close()
+    archi.close()
+
+def main() :
+    print("-"*20)
+    leer()
+    print("-"*20)
+
+if __name__ == "__main__" :
+    main()

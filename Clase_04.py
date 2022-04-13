c9dcf1c8fe77b8eea32a94cc95dcb94b7cd0008d
soyrockionic <soyrockionic@gmail.com>
2022-04-12 21:52:01 -0300

    modifico la funcion

--- Clase_04.py
+++ Clase_04.py
@@ -9,20 +9,30 @@ archi.write("70 50 37 60")
 arch.close()
 archi.close()
 
+
 def leer() : 
+    pro = open("promocionados.csv", "w")
+
     arch = open("Nombres.txt", "r")
     arch1 = arch.read().split()
     archi = open("Notas.txt", "r")
     archi1 = archi.read().split()
+
     for val1, val2 in zip(arch1, archi1) :
         print('{:<12}  {:<8}'.format(val1,val2))
+        if int(val2) >= 60 :
+            pro.write(val1+' ')
+
     arch.close()
     archi.close()
+    pro.close()
+
 
 def main() :
     print("-"*20)
     leer()
     print("-"*20)
+
 
 if __name__ == "__main__" :
     main()

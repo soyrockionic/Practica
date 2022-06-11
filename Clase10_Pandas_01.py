44c7235c160a208ce601019ded72c540746af443
soyrockionic <44347792+soyrockionic@users.noreply.github.com>
2022-06-11 16:12:47 -0300

    Pandas

---
+++ Clase10_Pandas_01.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+
+url = "https://es.wikipedia.org/wiki/Copa_Am%C3%A9rica"
+
+result = pd.read_html(url)
+
+
+ediciones = result[1]
+
+ediciones = ediciones.drop([4, 8, 12, 13], axis="columns")
+
+ediciones = ediciones[2:]
+
+ediciones.columns = ediciones.iloc[0]
+
+ediciones = ediciones.drop([31],axis="rows")
+
+print()
+print(ediciones)

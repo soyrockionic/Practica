0a460bd008bfe66882d0865a86df526300f7185d
soyrockionic <44347792+soyrockionic@users.noreply.github.com>
2022-06-16 23:53:59 -0300

    Pandas

---
+++ Clase10_Pandas_03.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+url = 'python_plus_acumulados.csv'
+
+datos = pd.read_csv(url,header = 0)
+
+# informo los primeros 10 ordenados alfabeticamente por nombre de mayor a menor
+print(datos.head(10).sort_values(by='Nombre',ascending=False))
+
+# informo nombre , apellido y puntaje total de los primeros 10
+print(datos[['Nombre','Apellido(s)','Total Python plus']].head(10))
+
+# informo los totales mayores a 70
+puntos = datos['Total Python plus']
+for i in puntos:
+    if(float(i.replace(',','.')) > 70):
+        print(i)
